10d8b0f3c6b3798f38ce00dadb5ba055135a1bc1
samfancy <samfancy2001@hotmail.com>
2025-11-03 18:57:38 +0000

    added the router

--- src/App.jsx
+++ src/App.jsx
@@ -6,73 +6,31 @@ import Greeting from './components/Greeting'
 import Counter from './components/Counter'
 import Users from './components/Users'
 import UserProfile from './components/UserProfile'
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Link, Route, Routes, useSearchParams } from 'react-router-dom'
 
 function App() {
-  const [currentView, setCurrentView] = useState('home');
-
-  const renderCurrentView = () => {
-    switch (currentView) {
-      case 'profile':
-        return <UserProfile />;
-      case 'users':
-        return <Users />;
-      case 'counter':
-        return <Counter />;
-      default:
-        return (
-          <>
-            <Greeting name="Samuel" />
-            <Counter />
-            <Users />
-          </>
-        );
-    }
-  };
-
   return (
     <>
       <div>
+         
         <BrowserRouter >
-          <nav >
-             <Link to="/">Home</Link>
-             <Link to="/profile">Profile</Link>
+        <main className="app-content">
+          <nav className="app-navigation">
+             <Link className='nav-btn active' to="/">Home</Link>
+             <Link className='nav-btn' to="/profile">Profile</Link>
+             <Link className='nav-btn' to="/users">Users</Link>
+             <Link className='nav-btn' to="/counter">Counter</Link>
           </nav>
+          </main>
           <Routes>
             <Route path='/' element={<Greeting name="Samuel" />} />
             <Route path='/profile' element={<UserProfile />} />
+            <Route path='/users' element={<Users/>} />
+            <Route path='/counter' element={<Counter/>} />
+            <Route path="*" element={<h2>404 - Page Not Found</h2>} />
           </Routes>
         </BrowserRouter>
-        {/* <nav className="app-navigation">
-          <button 
-            className={currentView === 'home' ? 'nav-btn active' : 'nav-btn'}
-            onClick={() => setCurrentView('home')}
-          >
-            Home
-          </button>
-          <button 
-            className={currentView === 'profile' ? 'nav-btn active' : 'nav-btn'}
-            onClick={() => setCurrentView('profile')}
-          >
-            Profile
-          </button>
-          <button 
-            className={currentView === 'users' ? 'nav-btn active' : 'nav-btn'}
-            onClick={() => setCurrentView('users')}
-          >
-            Users
-          </button>
-          <button 
-            className={currentView === 'counter' ? 'nav-btn active' : 'nav-btn'}
-            onClick={() => setCurrentView('counter')}
-          >
-            Counter
-          </button>
-        </nav>
         
-        <main className="app-content">
-          {renderCurrentView()}
-        </main> */}
       </div>
     </>
   )
